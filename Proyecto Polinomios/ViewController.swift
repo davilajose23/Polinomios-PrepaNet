9cebe1e390bbb007ee678be71ab7848b5d25c4a8
Jose Fernando <davila.jose23@gmail.com>
2016-04-13 16:45:46 -0500

    Add files

---
+++ Proyecto Polinomios/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  prueba pantallas
+//
+//  Created by Jose Fernando Dávila Orta on 09/03/16.
+//  Copyright © 2016 JoseFernandoDavila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Inicio"
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
